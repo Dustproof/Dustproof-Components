de53949498f258a7d470cf5f967595f52d353698
Jez <jez@dustproof.co>
2020-07-26 10:06:18 +0100

    Updated nav

--- Nav/js/scripts.js
+++ Nav/js/scripts.js
@@ -1,11 +1,27 @@
-function openNavMenu() {
-    var navMenu = document.getElementById('nav-menu-container');
+var navMenu = document.getElementById('nav-menu-container');
 
-    if ( $('#nav-menu-container').css('display') == "none") {
-        navMenu.style.display = "flex";
+function openNavMenu() {
+
+    if ( $('#nav-menu-container').css('display') == "flex") {
+        navMenu.style.display = "none";
     }
 
     else {
-        navMenu.style.display = "none";
+        navMenu.style.display = "flex";
     }
 }
+
+var showNavMenu = function() {
+
+    if (window.innerWidth > 768) {
+        navMenu.style.display = "flex";
+    }
+    
+    if (window.innerWidth < 768) {
+        navMenu.style.display = "none";
+    }
+
+};
+
+window.addEventListener("resize", showNavMenu);
+window.addEventListener("onload", showNavMenu);
